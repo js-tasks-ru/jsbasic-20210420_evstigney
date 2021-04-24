5975b419aed6aa429a68a15c4443d12a001c2614
Vasilisa <evstigney2010@gmail.com>
2021-04-24 15:23:03 +0300

    Решение задачи про валидацию имен

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,9 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  name = !!name && name.trim();
+
+  return (name.length >= 4) ? !name.includes(" ") : false;
 }
 
 function sayHello() {
