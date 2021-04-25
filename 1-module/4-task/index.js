b941f17745545efb65ec6883001e23b9fc50fe28
Vasilisa <evstigney2010@gmail.com>
2021-04-25 17:10:41 +0300

    поправила решение задачи про валидацию, решение задачи про спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,17 @@
+const spamWords = [`1xBet`, `XXX`];
+
 function checkSpam(str) {
-  // ваш код...
-}
+  let strLC = str.trim().toLowerCase();
+  if (!strLC) return false;
+  
+  let isSpam = false;
+
+  for(let i = 0; i < spamWords.length; i++) {
+    if (strLC.includes(spamWords[i].toLowerCase())) {
+      isSpam = true;
+      break;
+    }
+  }
+  
+  return isSpam;
+};
