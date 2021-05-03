29bf378f565fb457c03e03604661556bf8598352
Vasilisa <evstigney2010@gmail.com>
2021-05-03 22:14:48 +0300

    решение задачи про min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  const dataArr = str.split(' ')
+                     .map((item) => item.split(','))
+                     .flat()
+                     .filter((item) => !isNaN(item));
+  return {
+    min: Math.min(...dataArr),
+    max: Math.max(...dataArr),
+  }
 }
