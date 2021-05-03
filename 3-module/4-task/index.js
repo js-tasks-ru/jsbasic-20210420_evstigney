641a105a78f144fe7dbd4353ccbead9417a82861
Vasilisa <evstigney2010@gmail.com>
2021-05-03 21:47:53 +0300

    решение задачи про зарплаты пользователей

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
-  // ваш код...
+  const lineFeed = `\n`;
+  let salaryString = ``;
+  return users.filter((item) => item.age <= age)
+              .reduce((salaryString, user, index, arr) => {
+                salaryString += `${user.name}, ${user.balance}`;
+
+                if (index !== arr.length - 1) salaryString += `${lineFeed}`;
+                
+                return salaryString;
+              }, salaryString);
 }
