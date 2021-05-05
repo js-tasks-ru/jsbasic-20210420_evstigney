b3f840513c348c431bc98571cc791cf8100ae318
Vasilisa <evstigney2010@gmail.com>
2021-05-05 12:07:13 +0300

    внесла правки к задаче про зарплаты

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,12 +1,15 @@
 function showSalary(users, age) {
-  const lineFeed = `\n`;
-  let salaryString = ``;
+  const lineFeed = '\n';
   return users.filter((item) => item.age <= age)
-              .reduce((salaryString, user, index, arr) => {
-                salaryString += `${user.name}, ${user.balance}`;
+              .map(({name, balance} = {}, index, arr) => {
 
-                if (index !== arr.length - 1) salaryString += `${lineFeed}`;
+                if (!name || !balance) return;
+
+                let salaryString = `${name}, ${balance}`;
+
+                if (index !== arr.length - 1) salaryString += lineFeed;
                 
                 return salaryString;
-              }, salaryString);
+              })
+              .join('');
 }
