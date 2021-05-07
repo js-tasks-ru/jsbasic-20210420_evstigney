8e82ebd4b973e3c77589c2dc3d5f792e8758d6aa
Vasilisa <evstigney2010@gmail.com>
2021-05-08 00:19:25 +0300

    решение задачи про список друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  ul.insertAdjacentHTML('afterbegin',
+                        friends.map(({firstName, lastName}) => `<li>${firstName} ${lastName}</li>`)
+                               .join('')
+                        );
+  return ul;
 }
