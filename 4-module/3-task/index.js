9e0223407eaddc33070c8c921ad9febb34f55096
Vasilisa <evstigney2010@gmail.com>
2021-05-08 09:09:56 +0300

    решение задачи про таблицу

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,39 @@
 function highlight(table) {
-  // ваш код...
+  const columnValues = {
+    1: function age(elem) {
+
+        if (elem.textContent < 18) {
+          elem.closest('tr')
+              .style.textDecoration = 'line-through';
+        }
+        
+      },
+    2: function sex(elem) {
+        elem.closest('tr')
+            .classList
+            .add((elem.textContent === 'm') ? 'male' : 'female');
+      },
+    3: function status(elem) {
+        let isAvailable = elem.dataset.available;
+
+        if (isAvailable) {
+          elem.closest('tr')
+              .classList
+              .add((isAvailable === 'true') ? 'available' : 'unavailable');
+          return;
+        }
+
+        elem.closest('tr').hidden = true;
+      },
+  };
+
+  [...table.tBodies[0].rows].map((row) => {
+
+    for (let i = 1; i < row.cells.length; i++) {
+      let cell = [...row.cells][i];
+      columnValues[i](cell);
+    }
+
+  });
+
 }
